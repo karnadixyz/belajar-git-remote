82cab28e41b24957be182a0b62bc1a52c62a25f1
Karnadi <karnadixyz@gmail.com>
2023-10-21 10:19:04 +0700

    Mengubah about page

--- about.php
+++ about.php
@@ -1,2 +1,8 @@
 <h2>Halaman Tentang Kami</h2>
 <p>Kami adalah sebuah perusahaan yang bergerak di bidang...</p>
+<p>Ini adalah paragraph 3</p>
+<p>Ini adalah paragraph 4</p>
+<p>Ini adalah paragraph 5</p>
+<p>Ini adalah paragraph 6</p>
+<p>Ini adalah paragraph 7</p>
+<p>Ini adalah paragraph 8</p>
